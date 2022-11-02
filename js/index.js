fe89a1cbd63d288e98d136d049d5dececf07f01c
costabeatriz <113780157+costabeatriz@users.noreply.github.com>
2022-11-01 22:38:38 -0500

    11-01-2022

--- js/index.js
+++ js/index.js
@@ -4,110 +4,109 @@
 //4.Se errar, começa a desenhar-se o boneco (primeiro a cabeça, depois o tronco, de seguida pernas e braços e termina-se com olhos, nariz e boca);
 //5.O jogador perde se não conseguir identificar a palavra antes do desenho estar completo;
 
-let pets = [
-"cat",
-"dog",
-"snake",
-"hamster",
-"fish",
-"bird",
-"rabbits"
-]
-let count = 0;
-let chosenWord = '';
-let modifiedWord = '';
-let letters = null;
-
 const word = document.getElementById("word");
 const categoriesContainer = document.getElementById("categories-container");
-const userInputSection = document.getElementById("letter-input")
+let userInputSection = document.getElementById("letter-input")
 const gameButtons = document.getElementById("game-buttons");
 const startButton = document.getElementById("start-button");
 const resetButton = document.getElementById("reset-button");
 const textresult = document.getElementById("text-result");
 
-function startGame(){ 
-  pickRandomWord()
-}
+let pets = [
+  "cat",
+  "dog",
+  "snake",
+  "hamster",
+  "fish",
+  "bird",
+  "rabbits"
+  ]
+  let count = 0;
+  let chosenWord = '';
+  let letters = null;
+  let input = userInputSection.value;
 
+function startGame(){ 
+ displayWord()
+ userInputSection.removeAttribute('hidden');
+ resetButton.removeAttribute('hidden');
+}
 
 function pickRandomWord(){
-  chosenWord = modifiedWord = pets[Math.floor(Math.random() * pets.length)];
-  chosenWord = modifiedWord = chosenWord.toUpperCase();
+  chosenWord = pets[Math.floor(Math.random() * pets.length)];
+  chosenWord = chosenWord.toUpperCase();
+  
 }
 
-function displayWord(a){
-  a.innerHTML = '';
+function displayWord(){
   pickRandomWord();
-  for(let i = 0; i < chosenWord,length;i++){
-    let letters = document.createElement('span');
-    a.appendChil(letters)
+  console.log(chosenWord)
+  for(let i = 0; i < chosenWord.length;i++){
+  let letters = chosenWord.replace(/./g, '<span class="dashes"> _ </span>');
+  word.innerHTML = letters
   }
 }
+
 //Replace span for letter
-function replaceAsLetter(type){
+function replaceAsLetter(){
+  let letter = ''
   for( let i = 0; i < chosenWord.length; i++){
-    if(chosenWord[i] == type){
-      letters.children[i].textContent = type
+    if(chosenWord[i] == input){
+      letters.children[i].textContent = letter
     }
   }
 }
-//Verifify if userInput is valid
 
-function verifyLetter(letter){
-userInputSection = letter.value;
-let foundLetter = modifiedWord.indexOf(userInputSection)
+function verifyLetter(){
+let foundLetter = chosenWord.indexOf(input)
 if(foundLetter >=0){
-  modifiedWord = modifiedWord.replaceAll(userInputSection, "")
-  replaceAsLetter(userInputSection)
-  if(!modifiedWord.length){
-    endGame(letter, 0)
+  chosenWord = chosenWord.replaceAll(input, "")
+  replaceAsLetter()
+  if(!chosenWord.length){
+    endGame(count)
   }
 }
 else {
     count++;
-    hangman(count)
-    if (count > 5) {
-        endGame(letter,1)
+    hangman()
+    if (count > 6) {
+        endGame(count)
     }
 }
-// Reset the input value
-letter.value = "";
+userInputSection.value = "";
 }
 
-function endGame(letter,m){
+function endGame(m){
 let message = !m ? "Congratulations, you won the game": "You failed! Try again?"
 alert(message)
-userInputSection.addEventListener('click', displayItem)
-
 }
 function hangman(){
   switch (count) {
     case 1:
-      document.getElementById('quadro').style.visibility='visible'
+      document.getElementById('quadro').removeAttribute('hidden');
     case 2:
-      document.getElementById('quadro').style.visibility='hidden'
-      document.getElementById('head').style.visibility='visible'
+      document.getElementById('quadro').setAttribute('hidden', true);
+      document.getElementById('head').removeAttribute('hidden');
       break;
     case 3:
-      document.getElementById('head').style.visibility='hidden'
-      document.getElementById('body').style.visibility='visible'
+      document.getElementById('head').setAttribute('hidden', true);
+      document.getElementById('body').removeAttribute('hidden');
       break;
     case 4:
-      document.getElementById('body').style.visibility='hidden'
-      document.getElementById('arm1').style.visibility='visible'
+      document.getElementById('body').setAttribute('hidden', true);
+      document.getElementById('arm1').removeAttribute('hidden');
       break;
     case 5:
-      document.getElementById('arm1').style.visibility='hidden'
-      document.getElementById('arm2').style.visibility='visible'
+      document.getElementById('arm1').setAttribute('hidden', true);
+      document.getElementById('arm2').removeAttribute('hidden');
       break;
     case 6:
-      document.getElementById('arm2').style.visibility='hidden'
-      document.getElementById('leg1').style.visibility='visible'
+      document.getElementById('arm2').setAttribute('hidden', true);
+      document.getElementById('leg1').removeAttribute('hidden');
       break;
     case 7:
-      document.getElementById('leg1').style.visibility='hidden'
-      document.getElementById('leg2').style.visibility='visible'
+      document.getElementById('leg1').setAttribute('hidden', true);
+      document.getElementById('leg2').removeAttribute('hidden');
       break;
     default:
       break;
@@ -117,9 +116,9 @@ function resetGame(){
 count = 0;
 userInputSection = '';
 chosenWord = '';
-modifiedWord = '';
 }
 
 startButton.onclick = startGame
-userInputSection.addEventListener('click',verifyLetter())
+userInputSection.onkeyup = verifyLetter
+resetButton.onclick = resetGame
 
